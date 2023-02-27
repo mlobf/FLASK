08abc5de8e9d274cddd3ff7e68d4f684953e3a53
mlobf <marcoslemeborbafilho@gmail.com>
2023-02-27 01:14:09 -0300

    Fixing Endpoints

--- app.py
+++ app.py
@@ -6,11 +6,23 @@ import uuid
 app = Flask(__name__)
 
 
+# Store Get All Stores
 @app.get("/store")
 def get_stores():
-    return {"stores": list(stores.values())}
+    return stores, 201
 
 
+# Store Get Store by Id
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    store_id = request.get_json()
+    try:
+        return stores[store_id["id"]], 201
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+# Store Create Store
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -22,12 +34,30 @@ def create_store():
     for store in stores.values():
         if store_data["name"] == store["name"]:
             abort(400, message="This store already exists")
+
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
     return new_store, 201
 
 
+# Item get All Items
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+# Item get Item by ID
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    item_id = request.get_json()
+    try:
+        return items[item_id["id"]]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+# Item Create Item
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
@@ -51,35 +81,10 @@ def create_item():
                 message="Item already exist",
             )
 
-    if item_data not in stores:
-        abort(404, message="Store not found.")
-
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
     items[item_id] = new_item
     return new_item, 201
-
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-# get item in store
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,9 +1,2 @@
-stores = {
-    1: {"name": "My Store"},
-    2: {"name": "My Other Store"},
-}
-
-items = {
-    1: {"name": "Chair", "price": 17.99},
-    2: {"name": "Table", "price": 180.99},
-}
+stores = {}
+items = {}
