11203b117913a0e58482df135d4fbb176eb496c1
mlobf <marcoslemeborbafilho@gmail.com>
2023-02-28 01:02:26 -0300

    Add put store by id

--- app.py
+++ app.py
@@ -23,17 +23,50 @@ def get_store(store_id):
 
 
 # Store Put Store by Id
+""" 
+{
+	"3deed49d6f894e1c85d215e18dbbf31d": {
+		"id": "3deed49d6f894e1c85d215e18dbbf31d",
+		"name": "Gearonson"
+	},
+	"d719a21abc0741a281f385983fc4acc7": {
+		"id": "d719a21abc0741a281f385983fc4acc7",
+		"name": "Mappin"
+	},
+	"f4744098585041909b8defa41e9372aa": {
+		"id": "f4744098585041909b8defa41e9372aa",
+		"name": "Mesbla"
+	}
+}
+
+{'name': 'Mappin', 'id': '553e761b5d8441518cb6eac6e522e19d'}
+(Pdb) stores[_id].id
+*** AttributeError: 'dict' object has no attribute 'id'
+(Pdb) stores[_id]['id']
+'553e761b5d8441518cb6eac6e522e19d'
+(Pdb) stores[_id]['id']
+'553e761b5d8441518cb6eac6e522e19d'
+(Pdb) stores[_id]
+{'name': 'Mappin', 'id': '553e761b5d8441518cb6eac6e522e19d'}
+(Pdb) stores[_id] = stores_id
+*** NameError: name 'stores_id' is not defined
+(Pdb) stores[_id] = store_id
+(Pdb) stores
+{'553e761b5d8441518cb6eac6e522e19d': {'id': '553e761b5d8441518cb6eac6e522e19d', 'name': 'Americanas Falida'}}
+
+"""
+
+
 @app.put("/store/<string:store_id>")
 def put_store(store_id):
-    store_id = request.get_json()
-    store = stores[store_id["id"]]
-    return null
-    """ 
     try:
-        return stores[store_id["id"]], 201
-    except KeyError:
+        store_id = request.get_json()
+        _id = store_id["id"]
+        stores[store_id["id"]] = store_id
+        return stores, 201
+
+    except:
         abort(404, message="Store not found.")
-    """
 
 
 # Store Delete Store by Id
@@ -66,6 +99,7 @@ def create_store():
     return new_store, 201
 
 
+# ----------------------------------------------------------------------------------------------
 # Item get All Items
 @app.get("/item")
 def get_all_items():
@@ -82,6 +116,18 @@ def get_item(item_id):
         abort(404, message="Item not found.")
 
 
+""" 
+# Item Put Item
+@app.put("/item/<string:item_id>")
+def put_item_by_id(item_id):
+    item_id = reuquest.get_json()
+    if items[item_id["id"]]:
+        print("ook ")
+    return null
+"""
+
+
+# Item delete Item
 @app.delete("/item/<string:item_id>")
 def delete_item(item_id):
     item_id = request.get_json()
